673add61e0d5de6ad172d5d67c73e3806e899154
Krygin Ivan <krygin.ia@gmail.com>
2018-06-09 21:18:15 +0300

    date picker

---
+++ design/date-range-picker/src/main/java/com/travels/android/design/widget/DateRangePicker.kt
@@ -0,0 +1,39 @@
+package com.travels.android.design.widget
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.view.ViewGroup
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView.SELECTION_MODE_RANGE
+import java.util.*
+
+class DateRangePicker : DialogFragment() {
+
+    private lateinit var dateRangeSelectedListener: (from: Date, to: Date) -> Unit
+
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val materialCalendarView = MaterialCalendarView(context).apply {
+            selectionMode = SELECTION_MODE_RANGE
+            layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        }
+        return AlertDialog.Builder(context, theme)
+                .setView(materialCalendarView)
+                .setTitle("Select date range")
+                .setPositiveButton("Confirm", { dialog, which ->
+                    val selectedDates = materialCalendarView.selectedDates
+                    dateRangeSelectedListener(selectedDates.first().date, selectedDates.last().date)
+                })
+                .setNeutralButton("Cancel", { dialog, which -> })
+                .create()
+    }
+
+    fun setOnDateRangeSelectedListener(dateRangeSelectedListener: (from: Date, to: Date) -> Unit): DateRangePicker {
+        this.dateRangeSelectedListener = dateRangeSelectedListener
+        return this
+    }
+
+}
--- main/src/main/java/com/travels/android/main/search/filter/JourneysFilterActivity.kt
+++ main/src/main/java/com/travels/android/main/search/filter/JourneysFilterActivity.kt
@@ -1,16 +1,36 @@
 package com.travels.android.main.search.filter
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.jakewharton.rxbinding2.view.clicks
+import com.travels.android.design.widget.DateRangePicker
 import com.travels.android.main.R
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.activity_journeys_filter.*
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatter
 
-class JourneysFilterActivity: AppCompatActivity() {
+class JourneysFilterActivity : AppCompatActivity() {
+
+    val disposables = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_journeys_filter)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        disposables += date_range_text_view.clicks().subscribe {
+            DateRangePicker()
+                    .setOnDateRangeSelectedListener { from, to -> date_range_text_view.text =
+                            "From ${DateFormat.getDateInstance(DateFormat.SHORT).format(from)} to ${DateFormat.getDateInstance(DateFormat.SHORT).format(to)}" }
+                    .show(supportFragmentManager, null) }
     }
+}
+
+private operator fun CompositeDisposable.plusAssign(subscribe: Disposable) {
+    add(subscribe)
 }
