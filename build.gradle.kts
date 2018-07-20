67cc3090470ffd159ae921320169ca47b6638b12
ivan <krygin.ia@gmail.com>
2018-07-20 20:08:42 +0300

    graph

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,8 @@ import com.travels.android.build_src.plugins.AnyAndroidPlugin
 import com.travels.android.build_src.plugins.ApplicationPlugin
 import com.travels.android.build_src.util.withType
 import com.travels.android.build_src.util.android
+import com.vanniktech.dependency.graph.generator.DependencyGraphGeneratorExtension
+import com.vanniktech.dependency.graph.generator.DependencyGraphGeneratorExtension.Generator
 
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
@@ -18,6 +20,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:$gradlePluginVersion")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("com.vanniktech:gradle-dependency-graph-generator-plugin:0.5.0")
     }
 }
 
@@ -61,4 +64,15 @@ subprojects.withType<AnyAndroidPlugin> { project, plugin ->
             }
         }
     }
+}
+
+apply(plugin = "com.vanniktech.dependency.graph.generator")
+
+val projectLibraries = Generator(
+        name = "projectLibraries",
+        include = { dependency -> dependency.moduleGroup.startsWith(rootProject.name) }
+)
+
+configure<DependencyGraphGeneratorExtension> {
+    generators = arrayListOf(projectLibraries)
 }
